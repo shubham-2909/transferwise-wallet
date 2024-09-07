5c78cd97975052bd9173ec7a474fde39e35352e2
Shubham Gandhi <sg9769356@gmail.com>
2024-09-07 16:31:30 +0530

    generate prisma client before building

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -5,7 +5,6 @@ import { authOptions } from '@/lib/auth'
 import prisma from '@repo/db/client'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
-
 async function getBalance(userId: number) {
   const balance = await prisma.balance.findFirst({
     where: {
